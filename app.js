372ccf1524204529c8a875abc11be0a65599c9cb
Prasanna Balakrishnan <57198030+pb452@users.noreply.github.com>
2020-11-18 10:20:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var port = 9090;
+
+var url = "mongodb://localhost:27017/capstone";
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
+
+mongoose.connection;
+
+var Product = require("./router/product.router.js")
+
+var User = require("./router/user.router.js")
+
+var Brand = require("./router/brand.router.js")
+
+app.use("/product",Product);
+app.use("/user",User);
+app.use("/brand", Brand);
+
+app.listen(port,()=>console.log(`Server is running on port: ${port}`));
+
+
